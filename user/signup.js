e43b34b8d5cb63c38e1d3d0d77e45dd61985d1d8
TMDragom <tmdragomani@gmail.com>
2022-01-19 01:55:34 -0600

    debugged; not my own creation. Very loyal to tutorial

--- user/signup.js
+++ user/signup.js
@@ -1,36 +1,36 @@
 //save user info when they submit on signup.html and redirect to profile.html
 
 
-const signUpForm = document.forms.signUpForm
+const signUpForm = document.forms.SignUp;
 
 //create listener for forms submission event
 //alt syntax adds onsubmit listener directly in html
-signUpForm.addEventListener("submit", event => {
+signUpForm.addEventListener("submit", function(event) {
 
   //prevent page from refreshing
   event.preventDefault();
 
   //extract input data  //look-up alt ways to do this:
-  const profileName = signUpForm.elements.name.value;
-  const profession = signUpForm.elements.profession.value;
+  const name = signUpForm.elements.name.value;
+  const jobTitle = signUpForm.elements.profession.value;
 
   //use helper function to save user data
-  saveUserToLocalStorage(profileName, profession);
+  saveUserToLocalStorage(name, jobTitle);
 
   //redirect user to profile page (should be able to do in html also)
   window.location.href = "/user/profile.html";
 });
 
 //helper function- use function declaration
-function saveUserToLocalStorage(profileName, profession) {
-  const profile = {
-    "Profile Name": profileName,
-    "profession": profession //object shorthand notation
+function saveUserToLocalStorage(name, jobTitle) {
+  const profileJSON = {
+    "name": name,
+    "jobTitle": jobTitle
   }
+
+  //"Serialize" data: convert it into usable format for local storage (string)
+  const profileString = JSON.stringify(profileJSON);
+
+  //save user JSON to local storage using localStorage.setItem with key "_cb_user"
+  localStorage.setItem("_cb_user", profileString);
 };
-
-//"Serialize" data: convert it into usable format for local storage (string)
-const profileJSON = JSON.stringify(profile);
-
-//save user JSON to local storage using localStorage.setItem with key "_cb_user"
-localStorage.setItem("\_cb_user", profileJSON);
